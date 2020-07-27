a0267293e36a24cdbe6c45d8a60a4cdf1da8b72c
Hien Pham <phaminhien@hotmail.com>
2020-07-27 22:13:39 +0700

    search recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-// Global app controller
+import Search from './model/Search';
+import * as searchView from './view/searchView';
+import { elements, renderLoader, clearLoader } from './view/base';
+
+const state = {};
+
+const controlSearch = async () => {
+    const query = searchView.getInput();
+    if (query) {
+        state.search = new Search(query);
+        searchView.clearInput();
+        searchView.clearResult();
+        renderLoader(elements.searchResult);
+        await state.search.getResult();
+        clearLoader();
+        searchView.renderResult(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
